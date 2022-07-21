20ca8d410dcb12bc4cecef0525392904a78c1eb3
Digitexmedialab <101299324+Digitexmedialab@users.noreply.github.com>
2022-07-21 17:10:43 -0400

    Add files via upload

---
+++ secoes/footer.php
@@ -0,0 +1,65 @@
+<footer>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-2 mb-3 text-center d-flex justify-content-center align-items-center col-5">
+        <a class="navbar-brand js-scroll-trigger" href="#">
+            <img src="midia/img/logo.png" alt="logo <?php echo $nome_produto;?>">
+        </a>
+      </div>
+      <!---- / / ---->
+      <div class="col-md-2 mb-3">
+        <h4>LINKS</h4>
+        <a href="#perguntas" class="scroll">Perguntas frequentes</a>
+        <br>
+        <a href="politica-de-privacidade.php">Política de privacidade</a>
+        <br>
+        <a href="termos-de-uso.php">Termos e condições</a>
+        <br>
+        <a href="<?php echo $link_rastreio; ?>" class="rastreio">
+          RASTREAR PEDIDO
+        </a>
+      </div>
+      <!---- / / ---->
+      <div class="col-md-3 mb-3">
+        <h4>Aviso</h4>
+        <p>            
+          Não comercializamos o <?php echo $nome_produto;?> no Mercado Livre. A venda só pode ser realizada através deste Site Oficial e não nos responsabilizamos por compras realizadas em outros sites. Evite falsificações e riscos a sua saúde.
+        </p>
+      </div>
+      <!---- / / ---->
+      <div class="col-md-3 mb-3">
+        <h4>Formas de Pagamento</h4>
+        <img src="midia/img/pagamento-icon.png" width="200">
+      </div>
+      <!---- / / ---->
+      <div class="col-md-2 mb-3 p-lg-0">
+        <h4>Site seguro</h4>
+        <ul class="ssl">
+          <li>
+            <i aria-hidden="true" class="fab fa-expeditedssl"></i>
+            AMBIENTE SEGURO
+          </li>
+          <li>
+            <i aria-hidden="true" class="fas fa-shield-alt"></i>
+            SSL CERTIFICADO
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<div class="rodape">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6 my-3">
+        <a href="index.html" class="text-light"><?php echo $url;?> </a>
+        Desenvolvido por 
+        <a href="https://linktr.ee/lucas_grego" target="_blank" style="font-weight: 700; color:yellow;">&nbsp;<u>Luca.Web</u></a>
+      </div>
+      <div class="col-lg-6 col-sm-8 text-lg-right">
+        <b>Todos direitos reservados ®<?php echo $nome_produto;?> • <?php echo date('20y'); ?>
+        </b>
+      </div>
+    </div>
+  </div>
+</div>
